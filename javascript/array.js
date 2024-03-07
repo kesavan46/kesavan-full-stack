48c5c1b455199129fcc7568cb87cf84f7c1baa68
kesavan46 <kesavansubramani2002@gmail.com>
2024-03-07 09:54:41 +0530

    push

---
+++ javascript/array.js
@@ -0,0 +1,33 @@
+// let arr=["danny","doc","que","joe"]
+// console.log(arr.indexOf("joe"));
+// document.write(arr.indexOf("joe"));
+
+
+// let number=[1,2,3,4]
+
+// number.forEach(n =>console.log(n));
+// for(int =0;i<number.length;i++)
+// console.log(i)
+// number.forEach( function(n){
+//     console.log(n) 
+// });
+// let cities=[
+//     { name: "sruu", rivers: 3},
+//     { name: "koth", rivers: 4},
+//     { name: "sruu", rivers: 8},
+// ]
+// let morethantworivers= cities.filter(c =>c.rivers ==="koth")
+// let cities=[
+//         { item: "sruu", quantity: 3},
+//         { item: "koth", quantity: 4},
+//         { item: "sruu", quantity: 8},
+//     ]
+// let name=["danny","doc","que","joe"];
+// name.sort();
+// console.log(name);
+let name=["danny","doc","que","joe"];
+name.pop();
+console.log(name.pop());
+
+
+// let name1=["danny","doc","que","joe"];
